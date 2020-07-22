eebc0a79a0264dab007b93571e35047ebe2df278
Rishit Khare <kharerishit@gmail.com>
2020-07-22 16:19:23 -0700

    Initial Unity Project
    Empty Unity project with scripts

---
+++ Assets/Scripts/Non-monobehaviour/KinematicEquation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class KinematicEquation {
+    // Represents whether each quantity is known {Vi, Vf, Δt, a, ΔX}
+    private bool[] knownQuantities = new bool[5];
+    private string[] quantities = { "Vi", "Vf", "Δt", "a", "ΔX" };
+
+    protected Steps work;
+
+    protected Expression leftSide;
+    protected Expression rightSide;
+
+    protected int absentQuantityIndex = -1;
+
+    public KinematicEquation() {}
+
+    public static void main(string[] args) {
+
+    }
+}
