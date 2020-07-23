b914426d41ec482dfc6cec5a12515a2f342773cc
Rishit Khare <kharerishit@gmail.com>
2020-07-22 23:44:01 -0700

    Csharp porting (pt 2)

--- Assets/Scripts/Non-monobehaviour/KinematicEquation.cs
+++ Assets/Scripts/Non-monobehaviour/KinematicEquation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 public class KinematicEquation {
+
     // Represents whether each quantity is known {Vi, Vf, Δt, a, ΔX}
     private bool[] knownQuantities = new bool[5];
     private string[] quantities = { "Vi", "Vf", "Δt", "a", "ΔX" };
@@ -13,9 +14,8 @@ public class KinematicEquation {
 
     protected int absentQuantityIndex = -1;
 
-    public KinematicEquation() {}
+    public KinematicEquation() { }
 
-    //constructor takes the knowns and values
     public KinematicEquation(bool[] knowns, string[] values) {
         this.knownQuantities = knowns;
         this.quantities = values;
@@ -27,8 +27,131 @@ public class KinematicEquation {
         //(overidden by subclasses)
     }
 
+
     //*** ACCESSORS ***\\
 
     //accessor for work
-    public string GetWorkString() { return work.ToString(); }
+    public string GetWorkString() {
+        return work.ToString();
+    }
+
+    public Steps GetWork() { return work; }
+
+    //accessor method for all quantities
+    public string GetQuantity(int quantityIndex) {
+        return quantities[quantityIndex];
+    }
+
+    //accesses quantities but as double
+    public double GetNumericalQuantity(int quantityIndex) {
+        if (Algebra.IsNumber(quantities[quantityIndex])) {
+            return float.Parse(quantities[quantityIndex]);
+        }
+        throw new System.ArgumentException("ERROR: " + quantities[quantityIndex] + " is not a number");
+    }
+
+    public Expression GetLeftSide() {
+        return this.leftSide;
+    }
+
+    public Expression GetRightSide() {
+        return this.rightSide;
+    }
+
+    //accessor for knownQuantities
+    public bool[] GetKnownQuantities() {
+        return this.knownQuantities;
+    }
+
+    //accessor for quantities
+    public string[] GetQuantities() {
+        return quantities;
+    }
+
+    //counts number of 'true' in knownQuantities
+    public int NumberOfKnownQuantities() {
+        int count = 0;
+        for (int index = 0; index < knownQuantities.Length; index++) {
+            if (knownQuantities[index] && index != absentQuantityIndex) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    //*** MUTATORS ***\\
+
+    //sets quantity in array and updates knownQuantities
+    public void SetQuantity(int quantityIndex, string quantity) {
+        string quantityLower = quantity.ToLower(); //equalsIgnoreCase equivalent
+        if (!quantityLower.Equals("?")) {
+            quantities[quantityIndex] = quantity;
+            knownQuantities[quantityIndex] = true;
+        }
+    }
+
+    //mutator for work
+    public void SetWork(Steps work) {
+        this.work = work;
+    }
+
+    //mutator for leftSide
+    public void SetLeftSide(Expression leftSide) {
+        this.leftSide = leftSide;
+    }
+
+    //mutator for rightSide
+    public void SetRightSide(Expression rightSide) {
+        this.rightSide = rightSide;
+    }
+
+    //mutator for knownQuantities
+    public void SetKnownQuantities(bool[] knownQuantities) {
+        this.knownQuantities = knownQuantities;
+    }
+
+    //mutator for quantities
+    public void SetQuantities(string[] quantities) {
+        this.quantities = quantities;
+    }
+
+    //*** OTHER METHODS ***\\
+
+    //returns the answer by getting the last step of the work (ex: Vi = 5.0)
+    public string GetAnswer() {
+        return work.GetLastStep();
+    }
+
+    //checks for the first unknown on the list
+    public int GetMissingQuantityIndex() {
+        for (int quantityIndex = 0; quantityIndex < knownQuantities.Length; quantityIndex++) {
+            if (knownQuantities[quantityIndex] == false && quantityIndex != absentQuantityIndex) {
+                return quantityIndex;
+            }
+        }
+        throw new System.ArgumentException("ERROR: All values are known");
+    }
+
+    //Used for special case when solving quadratic equation in AIR
+    public bool IsTimeKnown() {
+        return knownQuantities[2];
+    }
+
+    //
+    public void CheckNumberOfQuantities(int quantities) {
+        if (NumberOfKnownQuantities() == 4) {
+            // if all four quantities of equation are known, then throw error (error may differ based on correctness of values given)
+            if (Algebra.isEqualEquation(leftSide, rightSide)) {
+                throw new System.ArgumentException("ERROR: Equation length - All quantities are known");
+            }
+            else {
+                throw new System.ArgumentException("ERROR: Equation length - Unequal equation");
+            }
+        }
+        if (NumberOfKnownQuantities() < 3) {
+            // if there isn't enough information, throw error
+            throw new System.ArgumentException("ERROR: Not enough information");
+        }
+    }
 }
--- Assets/Scripts/Non-monobehaviour/UnitConversion.cs
+++ Assets/Scripts/Non-monobehaviour/UnitConversion.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
+public class UnitConversion {
+    private UnitConversion displacementConversion;
+    private UnitConversion timeConversion;
 
-public class UnitConversion : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static readonly string[] validTimeUnits =
+        {"ps", "ms", "jiffy", "s", "sec", "min", "h", "hr", "hrs", "day", "wk",
+        "ftn", "mo", "yr"};
+    private static readonly float[] convertToSeconds = { 1E-12, 0.001, 0.01, 1,
+        1, 60, 3600, 3600, 3600, 86400, 604800, 1209600, 2.628E6, 3.154E7 };
+    private static readonly string[] validDisplacementUnits = {"in", "ft", "yd",
+        "ftbl", "fm", "cbl", "fur", "mi", "nmi", "nm", "mm", "cm", "m", "km",
+        "au", "lyr"};
+    private static readonly float[] convertToMeters = { 0.0254, 0.3048, 0.9144,
+        91.44, 1.8288, 185.2, 201.168, 1609.34, 1852, 1E-9, 0.001, 0.01, 1, 1000,
+        1.496E11, 9.461E15 };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int conversionIndex;
+
+    //prevents instantiation
+    private UnitConversion() { }
+
+
 }
