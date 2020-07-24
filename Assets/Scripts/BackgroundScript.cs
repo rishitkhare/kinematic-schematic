7a8ce7d057962b5bb29cd673be456fc93e9df44b
Rishit Khare <kharerishit@gmail.com>
2020-07-24 15:32:18 -0700

    Adding the ART

---
+++ Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    public GameObject[] elements;
+
+    public void ChangeBackground(int val) {
+        foreach(GameObject BG in elements) {
+            BG.SetActive(false);
+        }
+
+        elements[val].SetActive(true);
+    }
+}
