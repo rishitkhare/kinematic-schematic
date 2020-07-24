eddeeea5f25e517808def048d57e7bffcee74a5a
ComputerMasterHB <hisham.bhatti03@gmail.com>
2020-07-23 21:17:49 -0700

    Main Menu Completed

---
+++ Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+
+    public void PlayGame() {
+        SceneManager.LoadScene("Solve");    
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+}
